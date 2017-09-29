9e685eb098fa586a77ae58bb52d0f6144835c7d8
Jafet Benitez <jafet7g@gmail.com>
2017-09-28 22:58:09 -0400

    no message

--- test.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from sqlalchemy import create_engine
-
-engine = create_engine("postgresql://engine:1234567@localhost:5432/blueketchup")
-
-tags = pd.read_sql_query("select id from core_tags", con=engine)
-
-for index, tg in tags.iterrows():
-    
